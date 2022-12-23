3fa60506eac071b08a0ae6fa9ac1e67207e3afaf
jinwook567 <jinwook567@we2d.com>
2022-12-23 12:39:16 +0900

    feat: 로컬스토리지에 현재 URL 저장

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 let timer;
 let time = 500;
 
+const getCurrentURL = (event) => event.target.URL;
+
 window.addEventListener("scroll", (event) => {
   clearTimeout(timer);
   timer = setTimeout(() => {
     const y = window.scrollY;
-    const currentURL = event.target.URL;
-    console.log({ currentURL });
+    const currentURL = getCurrentURL(event);
+
+    window.localStorage.setItem(currentURL, y);
   }, time);
+});
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const currentURL = getCurrentURL(event);
+  const y = window.localStorage.getItem(currentURL);
+  scrollTo(y);
 });
