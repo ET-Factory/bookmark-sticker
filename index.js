6e0558baa223faa9f0d08eede8f4727bd75beba0
adultlee <seoungin1228@gmail.com>
2022-12-23 13:04:48 +0900

    feat: 로컬스토리지를 통한 스크롤 이동
    로컬스토리지를 통한 스크롤 이동을 수행합니다.

--- index.js
+++ index.js
@@ -1,20 +1,13 @@
-let timer;
-let time = 500;
-
 const getCurrentURL = (event) => event.target.URL;
 
-window.addEventListener("scroll", (event) => {
-  clearTimeout(timer);
-  timer = setTimeout(() => {
-    const y = window.scrollY;
-    const currentURL = getCurrentURL(event);
-
-    window.localStorage.setItem(currentURL, y);
-  }, time);
+window.addEventListener("load", (event) => {
+  const currentURL = getCurrentURL(event);
+  const scrollPositionY = Number(window.localStorage.getItem(currentURL));
+  if (scrollPositionY) window.scrollTo(0, y);
 });
 
-window.addEventListener("DOMContentLoaded", (event) => {
+window.addEventListener("unload", (event) => {
+  const scrollPositionY = window.scrollY;
   const currentURL = getCurrentURL(event);
-  const y = window.localStorage.getItem(currentURL);
-  scrollTo(y);
+  window.localStorage.setItem(currentURL, scrollPositionY);
 });
